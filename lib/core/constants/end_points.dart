841c515b81e453ad06e48ae20c1a576499625e91
Ahmed Omran <ahmedomran22.dev@gmail.com>
2026-07-12 11:32:19 +0300

    add endpoints to the app

--- lib/core/constants/end_points.dart
+++ lib/core/constants/end_points.dart
@@ -1,24 +1,19 @@
 abstract class EndPoints {
-  static const String baseUrl = 'https://re-viewers.com/api/v1';
-  static const String banners = '/sliders';
-  static const String categories = '/categories';
-  static const String categoryDetails = '/categories';
-  static const String latestProducts = '/products/latest';
-  static const String topRatedProducts = '/products/top-rated';
-  static const String mostViewedProducts = '/products/most-viewed';
-  static const String privacyPolicy = '/info/privacy';
-  static const String termsAndConditions = '/info/terms';
-  static const String faq = '/info/faq';
-  static const String aboutUs = '/info/about';
-  static const String contactSubjects = '/contact/subjects';
-  static const String contact = '/contact';
-  static const String login = "/login";
-  static const String register = "/register";
-  static const String updateProfile = "/profile/update";
-  static const String productDetails = '/products';
-  static const String addComment = '/reviews/product';
-  static const String updateComment = '/reviews/comment';
-  static const String deleteComment = '/reviews/comment';
-  static const String rateProduct = '/reviews/product';
-  static const String whatsapp = '/info/whatsapp';
+  static const String baseUrl = 'https://pharmacy-api.bya3online.com/api/v1';
+
+  // Authentication
+  static const String login = '/auth/login';
+  static const String customerRegister = '/auth/customer/register';
+  static const String me = '/auth/me';
+  static const String updateMe = '/auth/me';
+  static const String changePassword = '/auth/me/password';
+  static const String register = '/auth/register';
+  static const String refresh = '/auth/refresh';
+  static const String logout = '/auth/logout';
+
+  static String customerSocialLogin(String provider) =>
+      '/auth/customer/login/$provider';
+
+  static String customerSocialLoginCallback(String provider) =>
+      '/auth/customer/login/$provider/callback';
 }
