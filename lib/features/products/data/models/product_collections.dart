0ba885113e398605af756c1042cf1e93ecfdfa8a
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2026-07-19 13:29:21 +0300

    feat: Add ProductCollections class for product categorization

---
+++ lib/features/products/data/models/product_collections.dart
@@ -0,0 +1,7 @@
+abstract class ProductCollections {
+  static const String bestSeller = 'best_seller';
+  static const String topRated = 'top_rated';
+  static const String onSale = 'on_sale';
+  static const String newArrivals = 'new_arrivals';
+  static const String trending = 'trending';
+}
