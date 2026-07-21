c5b4a489e4b9bec0c5e61a4250d4c230bfb12b00
Ahmed Omran <ahmedomran22.dev@gmail.com>
2026-07-21 14:41:16 +0300

    feat: Remove CartRepo interface as part of cart feature refactor

--- lib/features/cart/data/repos/cart_repo.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import '../../../../core/errors/failure.dart';
-import '../models/add_to_cart_request_model.dart';
-
-abstract class CartRepo {
-  Future<Either<Failure, void>> addToCart(AddToCartRequestModel requestModel);
-}
